6cc4e8eca23ebb81315e8e99a9708f8fc2961381
Shinichi Ishimura <shiketaudonko41@gmail.com>
2016-08-07 00:21:44 +0900

    Separate test script from test runner

---
+++ test/test_install_and_use.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+TFENV_ROOT=$(cd $(dirname $0)/.. && pwd)
+export PATH="${TFENV_ROOT}/bin:${PATH}"
+
+rm -rf ${TFENV_ROOT}/versions
+
+error_versions=()
+for v in 0.{1..7}.0; do
+  echo "======================================================================"
+  tfenv install ${v}
+  tfenv use ${v}
+  if [ "$(terraform --version | grep -E "^Terraform v[0-9\.]+$")" != "Terraform v${v}" ]; then
+    error_versions+=( ${v} )
+  fi
+done
+
+if [ ${#error_versions[@]} -ne 0 ];then
+  echo "Following versions couldn't be installed properly" 1>&2
+  echo "${error_versions[@]}"
+  exit 1
+fi
