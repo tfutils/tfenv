ed2fb7a81a657871526ccd535acd336887c1343d
Shinichi Ishimura <shiketaudonko41@gmail.com>
2016-08-07 00:32:43 +0900

    Fix install_and_use test

--- test/test_install_and_use.sh
+++ test/test_install_and_use.sh
@@ -3,20 +3,19 @@
 TFENV_ROOT=$(cd $(dirname $0)/.. && pwd)
 export PATH="${TFENV_ROOT}/bin:${PATH}"
 
+available() {
+  v=${1}
+  [ "$(terraform --version | grep -E "^Terraform v[0-9\.]+$")" == "Terraform v${v}" ]
+}
+
+### Install specific version
 rm -rf ${TFENV_ROOT}/versions
 
-error_versions=()
-for v in 0.{1..7}.0; do
-  echo "======================================================================"
-  tfenv install ${v}
-  tfenv use ${v}
-  if [ "$(terraform --version | grep -E "^Terraform v[0-9\.]+$")" != "Terraform v${v}" ]; then
-    error_versions+=( ${v} )
-  fi
-done
-
-if [ ${#error_versions[@]} -ne 0 ];then
-  echo "Following versions couldn't be installed properly" 1>&2
-  echo "${error_versions[@]}"
+v=0.6.16
+tfenv install ${v}
+tfenv use ${v}
+available ${v}
+if ! available ${v}; then
+  echo "Installing specific version ${v} failed" 1>&2
   exit 1
 fi
