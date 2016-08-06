8d7822eec084fda4414242f313bedff30e9bce83
Shinichi Ishimura <shiketaudonko41@gmail.com>
2016-08-07 01:03:33 +0900

    Make reverse the result of list

--- test/test_list.sh
+++ test/test_list.sh
@@ -2,16 +2,17 @@
 
 rm -rf ${TFENV_ROOT}/versions
 
-for v in 0.6.1 0.6.2 0.6.16 0.7.0; do
+for v in 0.6.1 0.6.2 0.6.16 0.7.0-rc4 0.7.0; do
   tfenv install ${v}
 done
 
 result=$(tfenv list)
 expected="$(cat << EOS
-0.6.1
-0.6.2
-0.6.16
 0.7.0
+0.7.0-rc4
+0.6.16
+0.6.2
+0.6.1
 EOS
 )"
 if [ "${expected}" != "${result}" ]; then
