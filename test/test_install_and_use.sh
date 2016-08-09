89e3a0631ffc057eb8e5db3ea75a3b89fa8f66e3
Shinichi Ishimura <shiketaudonko41@gmail.com>
2016-08-09 19:02:27 +0900

    Install the version written in tfenv-version-file when version isn't specified

--- test/test_install_and_use.sh
+++ test/test_install_and_use.sh
@@ -1,23 +1,39 @@
 #!/usr/bin/env bash
 
-install_and_use() {
-  rm -rf ${TFENV_ROOT}/versions
+check_version() {
   v=${1}
-  tfenv install ${v}
-  tfenv use ${v}
   [ "$(terraform --version | grep -E "^Terraform v[0-9\.]+$")" == "Terraform v${v}" ]
 }
 
-### Install specific version
+echo "### Install latest version"
+rm -rf ${TFENV_ROOT}/versions
+rm -rf ${TFENV_ROOT}/.terraform-version
+
+if ! tfenv install; then
+  echo "Installing latest version ${v}" 1>&2
+  exit 1
+fi
+
+echo "### Install specific version"
+rm -rf ${TFENV_ROOT}/versions
+rm -rf ${TFENV_ROOT}/.terraform-version
+
 v=0.6.16
-if ! install_and_use ${v}; then
+tfenv install ${v}
+tfenv use ${v}
+if ! check_version ${v}; then
   echo "Installing specific version ${v} failed" 1>&2
   exit 1
 fi
 
-### Install latest version
+echo "### Install .terraform-version"
 rm -rf ${TFENV_ROOT}/versions
-if ! tfenv install; then
-  echo "Installing latest version ${v}" 1>&2
+rm -rf ${TFENV_ROOT}/.terraform-version
+
+v=0.6.15
+touch ${TFENV_ROOT}/.terraform-version && echo ${v} > ${TFENV_ROOT}/.terraform-version
+tfenv install
+if ! check_version ${v}; then
+  echo "Installing .terraform-version ${v}" 1>&2
   exit 1
 fi
