eb2bff5d78ffa5cd0b6b3c4ea54995642a745eb9
Shinichi Ishimura <shiketaudonko41@gmail.com>
2016-11-17 08:26:27 +0900

    Fix regex for beta releases

--- test/test_list.sh
+++ test/test_list.sh
@@ -3,12 +3,13 @@
 rm -rf ${TFENV_ROOT}/versions
 
 echo "### List local versions"
-for v in 0.6.1 0.6.2 0.6.16 0.7.0-rc4 0.7.0; do
+for v in 0.6.1 0.6.2 0.6.16 0.7.0-rc4 0.7.0 0.8.0-beta2; do
   tfenv install ${v}
 done
 
 result=$(tfenv list)
 expected="$(cat << EOS
+0.8.0-beta2
 0.7.0
 0.7.0-rc4
 0.6.16
