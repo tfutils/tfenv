20fa979ad70240586e6f6d9c8c01c8a59ba8db70
Shinichi Ishimura <shiketaudonko41@gmail.com>
2016-11-17 08:46:28 +0900

    Remove 0.6.1 to reduce testing time

--- test/test_list.sh
+++ test/test_list.sh
@@ -5,7 +5,7 @@
 rm -rf ${TFENV_ROOT}/versions
 
 echo "### List local versions"
-for v in 0.6.1 0.6.2 0.6.16 0.7.0-rc4 0.7.0 0.8.0-beta2; do
+for v in 0.6.2 0.6.16 0.7.0-rc4 0.7.0 0.8.0-beta2; do
   tfenv install ${v}
 done
 
@@ -16,7 +16,6 @@ expected="$(cat << EOS
 0.7.0-rc4
 0.6.16
 0.6.2
-0.6.1
 EOS
 )"
 if [ "${expected}" != "${result}" ]; then
