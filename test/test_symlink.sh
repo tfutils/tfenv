18c94fda22c2302f9a3a6f9f24264e0f056ce2a0
Shinichi Ishimura <shiketaudonko41@gmail.com>
2017-01-04 21:54:04 +0900

    Test symlinking more

--- test/test_symlink.sh
+++ test/test_symlink.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
 [ -n "$TFENV_DEBUG" ] && set -x
+source $(dirname $0)/helpers.sh
 
-TFENV_BIN_SYM=/tmp/tfenv
-rm -rf ${TFENV_BIN_SYM}
-ln -s ${PWD}/bin/tfenv ${TFENV_BIN_SYM}
+TFENV_BIN_DIR=/tmp/tfenv-test
+rm -rf ${TFENV_BIN_DIR} && mkdir ${TFENV_BIN_DIR}
+ln -s ${PWD}/bin/* ${TFENV_BIN_DIR}
+export PATH="${TFENV_BIN_DIR}:${PATH}"
 
 echo "### Test supporting symlink"
-${TFENV_BIN_SYM} --help
+cleanup
+tfenv install 0.8.2
+tfenv use 0.8.2
+check_version 0.8.2
