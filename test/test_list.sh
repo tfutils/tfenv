ad5a78f429cbb5b17a880f8f3f1108e9f6aea6fe
Shinichi Ishimura <shiketaudonko41@gmail.com>
2017-01-22 21:35:05 +0900

    Replace versions to reduce testing time

--- test/test_list.sh
+++ test/test_list.sh
@@ -6,17 +6,17 @@ source $(dirname $0)/helpers.sh
 echo "### List local versions"
 cleanup
 
-for v in 0.6.2 0.6.16 0.7.0-rc4 0.7.0 0.8.0-beta2; do
+for v in 0.6.16 0.7.0-rc4 0.7.2 0.7.13 0.8.0-beta2; do
   tfenv install ${v}
 done
 
 result=$(tfenv list)
 expected="$(cat << EOS
 0.8.0-beta2
-0.7.0
+0.7.13
+0.7.2
 0.7.0-rc4
 0.6.16
-0.6.2
 EOS
 )"
 if [ "${expected}" != "${result}" ]; then
