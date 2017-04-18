ffa0b5e55998d4914aca70d43a3fbe14ce3d4906
iamhsa <hugosalesses@gmail.com>
2017-04-18 17:00:29 +0200

    Refactor unit test for uninstall command (PR comment)

--- test/test_uninstall.sh
+++ test/test_uninstall.sh
@@ -19,13 +19,13 @@ source $(dirname $0)/helpers.sh \
 echo "### Uninstall local versions"
 cleanup || error_and_die "Cleanup failed?!"
 
-for v in 0.1.0; do
-  tfenv install ${v} || error_and_proceed "Install of version ${v} failed"
-done
+v=0.1.0
+(
+  tfenv install ${v} || exit 1
+  tfenv uninstall ${v} || exit 1
+  tfenv list | grep 0.1.0 && exit 1 || exit 0
+) || error_and_proceed "Uninstall of version ${v} failed"
 
-tfenv uninstall 0.1.0 || error_and_proceed "Uninstall of version ${v} failed"
-
-tfenv list | grep 0.1.0 && exit 1 || exit 0
 if [ ${#errors[@]} -gt 0 ]; then
   echo -e "\033[0;31m===== The following list tests failed =====\033[0;39m" >&2
   for error in "${errors[@]}"; do
