ca9922dd6781df02f4fbd292aa08e7ee5f7bdf7b
Mike Peachey <mike.peachey@bjss.com>
2026-04-24 16:30:06 +0100

    Show architecture in tfenv list output
    tfenv list now displays the architecture of each installed binary:
    
      * 1.5.0 (amd64) (set by /path/to/version)
        1.4.0 (arm64)
    
    Architecture is detected from the terraform binary using the file
    command. Supported architectures: amd64, arm64, 386, arm.
    
    Also updates the sed pattern in tfenv-uninstall to strip the new
    parenthesized arch from list output when matching versions.
    
    Fixes #321

--- test/test_list.sh
+++ test/test_list.sh
@@ -28,13 +28,22 @@ tfenv use 0.14.6;
 
 log 'info' '## Comparing "tfenv list" with default set';
 result="$(tfenv list)";
+
+# Determine expected arch from host platform
+case "$(uname -m)" in
+  x86_64)       expected_arch='amd64';;
+  aarch64|arm64) expected_arch='arm64';;
+  i386|i686)    expected_arch='386';;
+  *)            expected_arch='unknown';;
+esac;
+
 expected="$(cat << EOS
-* 0.14.6 (set by $(tfenv version-file))
-  0.9.11
-  0.9.2
-  0.9.1
-  0.7.13
-  0.7.2
+* 0.14.6 (${expected_arch}) (set by $(tfenv version-file))
+  0.9.11 (${expected_arch})
+  0.9.2 (${expected_arch})
+  0.9.1 (${expected_arch})
+  0.7.13 (${expected_arch})
+  0.7.2 (${expected_arch})
 EOS
 )";
 
